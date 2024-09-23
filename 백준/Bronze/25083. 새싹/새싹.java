c3432dce4d0074a553ddfa871497b5f570c64848
myeonghub <115239761+myeonghub@users.noreply.github.com>
2024-09-23 22:19:26 +0900

    [Bronze V] Title: 새싹, Time: 88 ms, Memory: 13872 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.print("         ,r'\"7\n");
+        System.out.print("r`-_   ,'  ,/\n");
+        System.out.print(" \\. \". L_r'\n");
+        System.out.print("   `~\\/\n");
+        System.out.print("      |\n");
+        System.out.print("      |\n");
+
+
+    }
+}
