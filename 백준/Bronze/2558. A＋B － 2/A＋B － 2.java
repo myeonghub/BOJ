46ad37cfb8e2a852cb92938eb806b621f308d291
myeonghub <115239761+myeonghub@users.noreply.github.com>
2024-10-27 20:22:54 +0900

    [Bronze V] Title: A+B - 2, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/2558. A＋B － 2/A＋B － 2.java
@@ -0,0 +1,15 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.math.BigInteger;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+       int n = Integer.parseInt(br.readLine());
+       int m = Integer.parseInt(br.readLine());
+
+        System.out.println(n+m);
+
+    }
+}
