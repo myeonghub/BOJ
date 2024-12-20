984c8232573745157e2d7c9e53f36d1a76fb6b71
myeonghub <115239761+myeonghub@users.noreply.github.com>
2024-12-20 16:19:31 +0800

    [Bronze IV] Title: 베라의 패션, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        
+        System.out.println(n*(n-1));
+
+
+    }
+}
+
+
