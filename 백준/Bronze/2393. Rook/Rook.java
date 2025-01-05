53590a570fd53ec98b17e7fa73fc868643bc8dc0
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-01-05 23:51:37 +0900

    [Bronze V] Title: Rook, Time: 100 ms, Memory: 13924 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.reflect.Parameter;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("  ___  ___  ___");
+        System.out.println("  | |__| |__| |");
+        System.out.println("  |           |");
+        System.out.println("   \\_________/");
+        System.out.println("    \\_______/");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |     |");
+        System.out.println("     |_____|");
+        System.out.println("  __/       \\__");
+        System.out.println(" /             \\");
+        System.out.println("/_______________\\");
+
+
+
+
+
+    }
+}
