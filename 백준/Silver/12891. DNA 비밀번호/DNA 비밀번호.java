395fbcd4799d1922a582f532540dd780711504bc
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-01-09 01:40:52 +0900

    [Silver II] Title: DNA 비밀번호, Time: 292 ms, Memory: 22152 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -4,7 +4,8 @@ import java.util.StringTokenizer;
 
 public class Main {
     static char[] ch = {'A', 'C', 'G', 'T'};
-    static char[] S ;
+    static char[] S;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -22,16 +23,17 @@ public class Main {
         for (int i = 0; i < 4; i++) {
             DNA[i] = Integer.parseInt(st1.nextToken());
         }
-        for (int i = 0; i <s; i++) {
-            S[i+1] = str.charAt(i);
+        for (int i = 0; i < s; i++) {
+            S[i + 1] = str.charAt(i);
         }
         int cnt = 0;
         int start = 1;
         int end = p;
-        for(int k=1;k<=p;k++){
+        // 초기 슬라이딩 윈도우 설정
+        for (int k = 1; k <= p; k++) {
             chk[sliding(k)]++;
         }
-        for(int i=p;i<=s;i++) {
+        for (int i = p; i <= s; i++) {
 
             boolean bool = false;
             //윈도우 크기를 만족할때, 해당 DNA가 유효한지 판단
@@ -41,18 +43,19 @@ public class Main {
                         bool = true;
                     }
                 }
-                if(!bool){
+                if (!bool) {
                     cnt++;
                 }
-                if(end==s) break;
-                chk[sliding(end+1)]++;
-                end++;
+                if (end == s) break; //end까지 진행시 break;
+
+                //슬라이딩윈도우
+                chk[sliding(++end)]++;
+
                 chk[backsliding(start++)]--;
-            }
-            else if ((end - start) < p - 1) {
-                chk[sliding(end+1)]++;
-                end++;
-            } else  {
+            } else if ((end - start) < p - 1) {
+                chk[sliding(++end)]++;
+
+            } else {
                 chk[backsliding(start++)]--;
             }
 
@@ -61,6 +64,7 @@ public class Main {
         System.out.println(cnt);
     }
 
+    //각 문자열 배열 값 추가
     static int checkDNA(char a) {
         for (int i = 0; i < 4; i++) {
             if (a == ch[i]) {
@@ -69,10 +73,12 @@ public class Main {
         }
         return -1;
     }
-    static int sliding(int a){
+
+    static int sliding(int a) {
         return checkDNA(S[a]);
     }
-    static int backsliding(int a){
+
+    static int backsliding(int a) {
         return checkDNA(S[a]);
     }
 }
