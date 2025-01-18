d8d4cee6e5dd50fa9bf2953a32e5bf3dd24e907f
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-01-18 23:46:13 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 100 ms, Memory: 13972 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuffer sb = new StringBuffer();
+
+        sb.append("       _.-;;-._\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-;;-._|\n" +
+                "'-..-'|   ||   |\n" +
+                "'-..-'|_.-''-._|");
+
+        System.out.println(sb);
+
+
+    }
+}
