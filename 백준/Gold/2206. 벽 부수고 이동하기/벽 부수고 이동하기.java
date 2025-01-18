e8028985813829ae0b97144eaf2611f88311cbe4
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-01-19 04:56:34 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 500 ms, Memory: 81808 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -6,12 +6,13 @@ import java.util.LinkedList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr ;
-    static boolean[][][] chk ;
-    static int[] dx = {-1,1,0,0};
-    static int[] dy = {0,0,-1,1};
+    static int[][] arr;
+    static boolean[][][] chk;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
-    static int n,m;
+    static int n, m;
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -23,54 +24,52 @@ public class Main {
         arr = new int[n][m];
 
         chk = new boolean[2][n][m];
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             String str = br.readLine();
-            for(int j=0;j<m;j++){
+            for (int j = 0; j < m; j++) {
                 arr[i][j] = str.charAt(j) - '0';
 
             }
         }
 
-        int dist = bfs(0,0);
+        int dist = bfs(0, 0);
 
-        
-        System.out.println(dist==0?-1:dist);
+
+        System.out.println(dist == 0 ? -1 : dist);
 
     }
 
 
-    public static int bfs(int a, int b){
-        ArrayDeque <Node> que = new ArrayDeque<>();
+    public static int bfs(int a, int b) {
+        ArrayDeque<Node> que = new ArrayDeque<>();
 
 
-        Node tmp ;
-        que.add(new Node(0,a,b,1));
+        Node tmp;
+        que.add(new Node(0, a, b, 1));
 
 
-        while (!que.isEmpty()){
+        while (!que.isEmpty()) {
             tmp = que.poll();
 
-            if(tmp.x==n-1 && tmp.y ==m-1){
+            if (tmp.x == n - 1 && tmp.y == m - 1) {
                 return tmp.d;
             }
 
-            for(int i=0;i<4;i++){
+            for (int i = 0; i < 4; i++) {
                 int x = dx[i] + tmp.x;
                 int y = dy[i] + tmp.y;
 
 
-
-                if(x<0 || y<0 || x>=n || y>=m){
+                if (x < 0 || y < 0 || x >= n || y >= m) {
                     continue;
                 }
 
-                if(arr[x][y] ==0 && !chk[tmp.c][x][y]){
+                if (arr[x][y] == 0 && !chk[tmp.c][x][y]) {
                     chk[tmp.c][x][y] = true;
-                    que.add(new Node(tmp.c,x,y,tmp.d+1));
-                }
-                else if(arr[x][y] == 1 && !chk[1][x][y] && tmp.c==0 ){
+                    que.add(new Node(tmp.c, x, y, tmp.d + 1));
+                } else if (arr[x][y] == 1 && !chk[1][x][y] && tmp.c == 0) {
                     chk[1][x][y] = true;
-                    que.add(new Node(1,x,y,tmp.d+1));
+                    que.add(new Node(1, x, y, tmp.d + 1));
                 }
             }
         }
@@ -81,12 +80,13 @@ public class Main {
     }
 
 
-    public static class Node{
+    public static class Node {
         int x;
         int y;
         int c;
         int d;
-        Node(int c,int x, int y, int d ){
+
+        Node(int c, int x, int y, int d) {
             this.x = x;
             this.y = y;
             this.c = c;
