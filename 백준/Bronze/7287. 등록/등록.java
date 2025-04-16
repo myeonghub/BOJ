514ef2cfd649e1ab61e5c4886aa2113c213e8451
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-04-16 23:58:54 +0900

    [Bronze V] Title: 등록, Time: 84 ms, Memory: 13728 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,11 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(298);
+        System.out.println("wlsaudwo");
+    }
+}
