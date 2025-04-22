ac9a9a0c81056fa8eb8f17c45ef9430828d8eec6
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-04-22 23:02:02 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 128 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,33 +1,39 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int [] dp = new int [n];
-        int [] arr = new int[n];
+        int N = Integer.parseInt(br.readLine());
+
+        int[] arr = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for(int i=0;i<n;i++){
+        for(int i = 0; i < N; i++){
             arr[i] = Integer.parseInt(st.nextToken());
-            dp[i]=1;
-        }
-        int max=1;
-        for (int j =0 ; j<n ; j++){
-            for(int k=0; k<j;k++){
-                if(arr[j]>arr[k]){
-                    dp[j]=Math.max(dp[j],dp[k]+1);
-                }
-            }
-            max = Math.max(max,dp[j]);
         }
 
-        System.out.println(max);
-
+        System.out.println(solution(arr,N)+1);
 
     }
 
+    static int solution(int[] arr, int N){
+
+        int cnt = 0;
+
+        int[] dp = new int[N];
+
+        for(int i = 0; i < N; i++){
+            for(int j = 0; j < i; j++){
+                if(arr[i] > arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+                cnt = Math.max(cnt,dp[i]);
+            }
+        }
+
+        return cnt;
+    }
 }
