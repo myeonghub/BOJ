4c91188118b0158a7a04f3155359bc2e1e105b7c
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-04-27 23:32:25 +0900

    [Bronze V] Title: 오늘 날짜, Time: 92 ms, Memory: 13916 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+public class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println("2025-04-27");
+    }
+}
