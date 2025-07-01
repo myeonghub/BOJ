166f2f87193879d9339489c8d020ce13787b89ee
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-07-01 17:34:20 +0900

    [Silver III] Title: N과 M (1), Time: 1824 ms, Memory: 68632 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,40 +1,41 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int N,M;
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        int M = Integer.parseInt(st.nextToken());
 
         int[] arr = new int[N+1];
-        boolean [] visited = new boolean[N+1];
+        boolean[] visit = new boolean[N+1];
 
-        dfs(0,arr,visited);
-
-
+        solution(0,N,M,arr,visit);
     }
-    public static void dfs(int k , int[] arr , boolean[] visited){
-       if(k ==M){
-           for (int i = 0; i < M; i++) {
-               System.out.print(arr[i] + " ");
-           }
-           System.out.println();
-           return;
-       }
-        for (int i = 1; i <= N; i++) {
-            if (visited[i])
-                continue;
-            visited[i] = true;
-            arr[k] = i;
-            dfs(k + 1,arr,visited);
-            visited[i] = false;
+    static void solution (int k, int N ,int M , int[] arr , boolean[] visit){
+        if(k == M){
+            for(int i = 0; i < M; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+            return;
         }
 
+        for(int i = 1; i <= N; i++){
+            if(visit[i]){
+                continue;
+            }
+            visit[i] = true;
+            arr[k] = i;
+            solution(k+1,N,M,arr,visit);
+            visit[i] = false;
+        }
     }
 }
