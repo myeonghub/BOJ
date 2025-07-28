87c494ee325b23ac13d33fcd35fc0c1b5655f573
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-07-28 22:44:45 +0900

    [Gold IV] Title: 알고스팟, Time: 124 ms, Memory: 14664 KB -BaekjoonHub

--- 백준/Gold/1261. 알고스팟/알고스팟.java
+++ 백준/Gold/1261. 알고스팟/알고스팟.java
@@ -1,92 +1,94 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.*;
 
 class Node implements Comparable<Node>{
-	int x;
-	int y;
-	int wall;
-		
-	public Node(int x, int y, int wall) {
-		this.x = x;
-		this.y = y;
-		this.wall = wall;
-	}
+    int y;
+    int x;
+    int wall;
 
-	@Override 
-	public int compareTo(Node o) {
-		// 오름차순 
-		return this.wall - o.wall;
-	}
+    public Node(int y, int x, int wall) {
+        this.y = y;
+        this.x = x;
+        this.wall = wall;
+    }
+
+    @Override
+    public int compareTo(Node o) {
+        // 오름차순
+        return wall - o.wall;
+    }
 }
-	
 public class Main {
-	
-	static int n,m;
-	static int[][] map;
-	static int res = Integer.MAX_VALUE;
-	
-	static int[] dx = {-1,1,0,0};
-	static int[] dy = {0,0,-1 ,1};
+    static int N;
+    static int M;
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st =new StringTokenizer(br.readLine());
-		
-		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		
-		map = new int[m][n];
-		
-		for(int i=0; i<m; i++) {
-			String[] line = br.readLine().split("");
-			for(int j=0; j<n; j++) {
-				int num = Integer.parseInt(line[j]);
-				map[i][j] = num;
-			}
-		}
-		
-		System.out.println(bfs(0,0));
-	}
-	
-	
-	
-	static int bfs(int x, int y) {
-		Queue<Node> q = new PriorityQueue<>();
-		boolean[][] check = new boolean[m][n];
-		
-		check[y][x] = true;
-		q.add(new Node(x,y,0));
-		
-		while(!q.isEmpty()) {
-			Node pos = q.poll();
-			
-			int px = pos.x;
-			int py = pos.y;
-			int pwall = pos.wall;
-            
-			// 도착
-			if(px == n-1 && py == m-1) {
-				return pwall;
-			}
-			
-			for(int i=0; i<4 ; i++) {
-				int nx = px+dx[i];
-				int ny = py+dy[i];
-				
-				if(nx <0 || ny <0 || nx>n-1 || ny >m-1) continue;
-				
-				if(check[ny][nx]) continue;
-				
-				check[ny][nx] =true;
-				if(map[ny][nx]==0) {
-					q.add(new Node(nx,ny, pwall));
-				}else {
-					q.add(new Node(nx,ny, pwall+1));
-				}
-			}
-			
-		}
-		return 0;
-		
-	}
+    static int[] X = {0,0,-1,1};
+    static int[] Y = {1,-1,0,0};
+
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+
+        int[][] arr = new int[N][M];
+
+        boolean[][] visit = new boolean[N][M];
+
+
+        for(int i = 0; i < N; i++){
+            String str = br.readLine();
+            for(int j = 0; j < M; j++) {
+                arr[i][j] = str.charAt(j) - '0';
+            }
+        }
+
+
+        System.out.println(bfs(arr,visit));
+
+
+    }
+    static int bfs (int[][] arr , boolean[][] visit) {
+        PriorityQueue<Node> que = new PriorityQueue<>();
+        visit[0][0] = true;
+
+        que.add(new Node(0,0,0));
+
+        while(!que.isEmpty()){
+            Node pos = que.poll();
+
+            int px = pos.x;
+            int py = pos.y;
+            int pwall = pos.wall;
+
+            if(py == N - 1 && px == M - 1){
+                return pos.wall;
+            }
+
+            for(int i = 0; i < 4; i++){
+                int b = Y[i] + py;
+                int a = X[i] + px;
+                if(b < 0 || a < 0 || a >= M || b >= N){
+                    continue;
+                }
+                if(arr[b][a] == 1 && !visit[b][a]){
+                    visit[b][a] = true;
+                    que.add(new Node(b,a,pwall + 1));
+                }
+                else if(arr[b][a] == 0 && !visit[b][a]){
+                    visit[b][a] = true;
+                    que.add(new Node(b,a,pwall));
+                }
+            }
+        }
+
+        return 0;
+
+    }
+
 }
+
