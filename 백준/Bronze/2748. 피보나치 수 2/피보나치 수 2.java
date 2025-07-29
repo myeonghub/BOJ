92e7e56c34237ae50cb66a3fb2c3d7e33b85bbc4
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-07-29 16:14:07 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main (String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        if(n ==0) {
+            System.out.println(0);
+            return;
+        }
+        System.out.println(fibo(n));
+    }
+
+    static long fibo(int n){
+        long[] arr = new long[n+1];
+
+        arr[0] = 0;
+        arr[1] = 1;
+
+        for(int i=2;i<=n;i++){
+            arr[i] = arr[i-1] + arr[i-2];
+        }
+
+        return arr[n];
+    }
+}
