585cee75a6c4ed612b7a78f3ebca46d6e0921f30
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-08-24 23:23:48 +0900

    [Silver IV] Title: 암기왕, Time: 1560 ms, Memory: 230548 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,67 +1,98 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.math.BigInteger;
+import java.util.*;
+
+
+//class Node implements Comparable<Node>{
+//    int y;
+//    int x;
+//    int wall;
+//
+//    public Node(int y, int x, int wall) {
+//        this.y = y;
+//        this.x = x;
+//        this.wall = wall;
+//    }
+//
+//    @Override
+//    public int compareTo(Node o) {
+//        // 오름차순
+//        return wall - o.wall;
+//    }
+//}
+
 
 public class Main {
+    static int n , p, q;
+    static int m , k;
+    static Long x , y;
+    static int cnt;
+
+    static int paint;
+    static int area;
+
+
+
+    static int[] X = {0,0,-1,1};
+    static int[] Y = {1,-1,0,0};
+
+
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
 
-        int t = Integer.parseInt(br.readLine());
+        int c = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
-        for(int c=0;c<t;c++) {
-            int n = Integer.parseInt(br.readLine());
+        for(int q = 0 ; q < c; q++) {
+            n = Integer.parseInt(br.readLine());
 
-            int[] arr = new int[n];
+
+            int[] a = new int[n];
+
             StringTokenizer st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < n; i++) {
-                arr[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0 ; i < n; i++){
+                a[i] = Integer.parseInt(st.nextToken());
             }
 
-            int m = Integer.parseInt(br.readLine());
-            int[] arr2 = new int[m];
-
+            k = Integer.parseInt(br.readLine());
+            int[] b = new int[k];
             st = new StringTokenizer(br.readLine());
-            for (int i = 0; i < m; i++) {
-                arr2[i] = Integer.parseInt(st.nextToken());
+            for(int i = 0 ; i < k; i++){
+                b[i] = Integer.parseInt(st.nextToken());
             }
 
-            Arrays.sort(arr);
 
-            boolean[] binary = new boolean[m];
 
-            for(int i=0;i<m;i++){
+            Arrays.sort(a);
+
+            for(int i = 0; i < k; i++) {
                 int start = 0;
-                int end = arr.length-1;
-
-                while(start<=end){
-                    int mid = (start+end)/2;
-
-                    if(arr2[i]>arr[mid]){
-                        start = mid+1;
-                    }
-                    else if(arr2[i]<arr[mid]){
-                        end = mid-1;
-                    }
-                    else{
-                        binary[i] = true;
+                int end = a.length - 1;
+                boolean chk = true;
+                while (start <= end){
+                    int mid = (start + end)/2;
+                    if(a[mid] == b[i]){
+                        sb.append(1 + "\n");
+                        chk = false;
                         break;
                     }
+                    else if(a[mid] < b[i]){
+                        start = mid + 1;
+                    }
+                    else{
+                        end = mid - 1;
+                    }
+                }
+                if(chk){
+                    sb.append(0 + "\n");
                 }
             }
+            
 
-            for(int i=0;i<m;i++){
-                if(binary[i]){
-                    sb.append(1 +"\n");
-                }
-                else{
-                    sb.append(0 +"\n");
-                }
-            }
         }
-
-
         System.out.println(sb);
     }
 }
+
