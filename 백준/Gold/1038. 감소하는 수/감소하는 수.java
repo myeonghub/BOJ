5e586238184c8d10b9946cd22617dae0f3250f3f
myeonghub <115239761+myeonghub@users.noreply.github.com>
2025-09-04 18:27:42 +0900

    [Gold V] Title: 감소하는 수, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,43 +1,79 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.LinkedList;
-import java.util.List;
+import java.math.BigInteger;
+import java.util.*;
+
+
+//class Node implements Comparable<Node>{
+//    int y;
+//    int x;
+//    int wall;
+//
+//    public Node(int y, int x, int wall) {
+//        this.y = y;
+//        this.x = x;
+//        this.wall = wall;
+//    }
+//
+//    @Override
+//    public int compareTo(Node o) {
+//        // 오름차순
+//        return wall - o.wall;
+//    }
+//}
+
 
 public class Main {
-    static List<Long> list = new ArrayList<>();
+    static int n , p, q;
+    static int m , k;
+    static Long x , y;
+    static int cnt;
+
+    static int paint;
+
+
+
+
+    static int[] X = {0,0,-1,1};
+    static int[] Y = {1,-1,0,0};
+
+    static List<Long>  list = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        if (n <= 10) {
+        StringBuilder sb = new StringBuilder();
+
+        n = Integer.parseInt(br.readLine());
+
+        if(n <= 10 ){
             System.out.println(n);
             return;
-        } else if (n >= 1023) {
+        }
+        else if(n >= 1023){
             System.out.println(-1);
             return;
         }
-        for (int i = 0; i < 10; i++) {
+        for(int i = 0 ; i < 10; i++){
             solution(i);
         }
         Collections.sort(list);
         System.out.println(list.get(n));
-    }
 
-    static void solution(long n) { // 감소하는수를 만들어냄
+    }
+    static void solution(long n){
         list.add(n);
         long num = n % 10;
-        if (num == 0) {
-            return;
+        if(num == 0){
+            return ;
         }
-        for (long i = num - 1; i >= 0; i--) {
+        for(long i = num - 1; i >= 0 ; i--){
             long tmp = n * 10 + i;
             solution(tmp);
         }
-
-
     }
 
+
+
 }
+
